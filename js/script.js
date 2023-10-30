cfe3116f7232a14da4aae5f95f51fb1cf5af5f70
Lúcia Duarte <analuciacfduarte@gmail.com>
2023-10-30 21:26:59 +0000

    adding some logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 console.log('Hello, there! 👋');
 console.log('We are starting the loop');
 console.log('Starting the for loop:');
+console.log('Showing how git works');
 
 for (let i = 0; i < 10; i++) {
   console.log(`This is iteration number ${i}.`);
