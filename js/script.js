bf07bdf173a4341c39a0bb9f4a00d06cca137613
Lúcia Duarte <analuciacfduarte@gmail.com>
2023-10-30 17:44:35 +0000

    small change in wording

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 console.log('Hello, there! 👋');
-console.log('Starting the loop:');
+console.log('We are starting the loop');
 
 for (let i = 0; i < 10; i++) {
   console.log(`This is iteration number ${i}.`);
